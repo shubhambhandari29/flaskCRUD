e3be873de13a2385e4b956f4b6554c637a03d08d
shubhambhandari29 <shubh.gehu@gmail.com>
2024-12-24 18:33:15 +0530

    flask app added

--- application/run.py
+++ application/run.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 
-flask_app = create_app()
+app = create_app()
 
 if __name__== '__main__':
-    flask_app.run('0.0.0.0',port=5000, debug=True)
+    app.run('0.0.0.0',port=5000, debug=True)
